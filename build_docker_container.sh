ab18a2be24f020c3dc9e671bdf8476c65bc2657d
Frits Hoogland <fhoogland@yugabyte.com>
2023-03-15 13:43:21 +0100

    and another variable fix

--- build_docker_container.sh
+++ build_docker_container.sh
@@ -12,7 +12,7 @@ CARGO_APP_VERSION=$(grep ^version Cargo.toml | sed 's/.*"\(.*\)"/\1/')
 # 3. build using docker file
 docker build --rm --tag yb_stats:${CARGO_APP_VERSION} --tag yb_stats:latest --file Dockerfile.container .
 BUILD_RETURN=$?
-if [ BUILD_RETURN -gt 0 ]; then
+if [ $BUILD_RETURN -gt 0 ]; then
   echo "docker build not successful."
   exit 1
 fi
